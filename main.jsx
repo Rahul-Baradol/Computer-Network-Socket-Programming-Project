250f921ee7d09d6bd682bc5df09e0208049fa3fe
Ranjitha <cgowliranjitha@gmail.com>
2024-02-17 03:09:42 +1100

    change the content in App.jsx and App.css and main.jsx

---
+++ main.jsx
@@ -0,0 +1,13 @@
+// index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Create from './App';
+//import Login from './login.jsx'
+
+ReactDOM.render(
+  <React.StrictMode>
+   < Create/>
+
+  </React.StrictMode>,
+  document.getElementById('root')
+);
